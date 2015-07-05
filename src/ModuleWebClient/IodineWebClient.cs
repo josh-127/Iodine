4b00845cb0f6328a38b69c0e0f7120f5335bb33b
Splitty <splittydev@gmail.com>
2015-07-05 21:07:05 +0200

    Added WebClient module

---
+++ src/ModuleWebClient/IodineWebClient.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using Iodine;
+
+namespace ModuleWebClient
+{
+	public class IodineWebClient : IodineObject
+	{
+		private static IodineTypeDefinition WebClientTypeDef =
+			new IodineTypeDefinition ("WebClient");
+
+		private WebClient client;
+
+		public IodineWebClient () : base (WebClientTypeDef) {
+			WebProxy proxy = new WebProxy ();
+			this.client = new WebClient ();
+			this.client.Proxy = proxy;
+		}
+
+		private IodineObject downloadString (VirtualMachine vm, IodineObject self, IodineObject[] args) {
+			IodineString uri = args [0] as IodineString;
+			string data;
+			try {
+				data = this.client.DownloadString (uri.ToString ());
+			} catch (Exception e) {
+				vm.RaiseException (e.Message);
+				return null;
+			}
+			return new IodineString (data);
+		}
+	}
+}
+
---
+++ src/ModuleWebClient/WebClientModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using Iodine;
+
+namespace ModuleWebClient
+{
+	[IodineExtension ("webclient")]
+	public class WebClientModule : IodineModule
+	{
+		public WebClientModule () : base ("webclient") {
+			this.SetAttribute ("WebClient", new InternalMethodCallback (webclient, this));
+		}
+
+		private IodineObject webclient (VirtualMachine vm, IodineObject self, IodineObject[] args)
+		{
+			return new IodineWebClient ();
+		}
+	}
+}
+
