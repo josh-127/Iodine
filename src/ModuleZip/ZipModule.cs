2110701bc79ad1ac707d1f8476c1c8958d85ce3f
Aurora01 <aurora@int0x10.com>
2015-07-05 17:46:20 +0100

    Added ziplib

---
+++ src/ModuleZip/ZipModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using Iodine;
+using Internals;
+
+namespace ModuleZip
+{
+
+	[IodineExtensionAttribute ("ziplib")]
+	public class ZipModule : IodineModule
+	{
+		public ZipModule () : base ("ziplib")
+		{
+			this.SetAttribute ("unzip", new InternalMethodCallback (unzip ,this));
+		}
+
+		private IodineObject unzip (VirtualMachine vm, IodineObject self, IodineObject[] args)
+		{
+			var archiveName = args [0] as IodineString;
+			var unzipper = new Unzip (archiveName.Value);
+			return new IodineUnzipper(unzipper);
+		}
+
+	}
+}
