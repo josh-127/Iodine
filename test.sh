41a91ce30ec6c048a500f5bffabca8d2ebf35622
GruntTheDivine <sloancrandell@gmail.com>
2015-08-31 03:06:40 -0400

    Updated test.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 git clone https://github.com/IodineLang/Ion
 mono -V
 cd tests
+module_path=$(readlink -f ../modules)
+export IODINE_PATH="$IODINE_PATH:$module_path"
 mono ../bin/iodine.exe ../Ion/ion_frontend.id install-deps
 mono ../bin/iodine.exe test.id
