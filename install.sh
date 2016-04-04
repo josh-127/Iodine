38349b71941449181858430cdbfcc197059c8f19
GruntTheDivine <sloancrandell@gmail.com>
2016-04-04 09:47:29 -0400

    Fixed install script

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ echo "Using prefix $prefix"
 mkdir -p $prefix/iodine
 mkdir -p $prefix/iodine/bin
 cp ./bin/iodine.exe $prefix/iodine/bin/iodine.exe
-cp ./bin/iodine.dll $prefix/iodine/bin/iodine.dll
+cp ./bin/LibIodine.dll $prefix/iodine/bin/LibIodine.dll
 cp -r ./modules $prefix/iodine/bin
 cp -r ./bin/extensions $prefix/iodine/bin
 cat ./iodine_run.sh > /usr/bin/iodine
