cdbb75c3371d542c238db5b60921056edd8aabd2
GruntTheDivine <sloancrandell@gmail.com>
2016-05-18 13:44:16 -0400

    Attempt to fix unit tests

--- test.sh
+++ test.sh
@@ -1,10 +1,8 @@
-git clone https://github.com/IodineLang/Ion
 mono -V
 ./build_iodine.sh
 cd tests
 module_path=$(readlink -f ../modules)
 echo $module_path
 export IODINE_PATH=$module_path
-mono ../bin/iodine.exe ../Ion/ion_frontend.id install-deps
 mono ../bin/iodine.exe test.id
 mono ../bin/iodine.exe jsontest.id
