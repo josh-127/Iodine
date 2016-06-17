0666944297060ffa059e1c8c9364004469aa1910
GruntTheDivine <sloancrandell@gmail.com>
2016-06-17 19:10:10 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ if [ "$(id -u)" != "0" ]; then
 	echo "WARNING: Not running as root, default installation directory WILL FAIL!"
 fi
 
-prefix="/usr/lib"
+prefix="/usr/lib/local"
 
 if [ "$#" -ge 1 ]; then
 	prefix=$1
@@ -13,12 +13,15 @@ fi
 echo "Using prefix $prefix"
 
 mkdir -p $prefix/iodine
-mkdir -p $prefix/iodine/bin
-cp ./bin/iodine.exe $prefix/iodine/bin/iodine.exe
-cp ./bin/LibIodine.dll $prefix/iodine/bin/LibIodine.dll
-cp -r ./modules $prefix/iodine/bin
-cp -r ./bin/extensions $prefix/iodine/bin
-cat ./iodine_run.sh > /usr/bin/iodine
+
+cp ./bin/iodine.exe $prefix/iodine/iodine.exe
+cp ./bin/LibIodine.dll $prefix/iodine/LibIodine.dll
+cp -r ./modules $prefix/iodine/
+cp -r ./bin/extensions $prefix/iodine
+
+echo "#! /bin/bash" > /usr/bin/iodine
+echo "/usr/bin/mono $prefix/iodine/iodine.exe \"\$@\"" >> /usr/bin/iodine
+
 echo -n "export IODINE_HOME=$prefix/iodine" | sudo tee /etc/profile.d/iodine.sh
 chmod a+x /usr/bin/iodine
 
